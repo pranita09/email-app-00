c6a9ab2e7f20b3423d4bbaa7c33c2ea700b58bbe
Pranita <pfulsundar8@gmail.com>
2023-04-23 04:33:34 +0530

    add: inbox page done

--- src/contexts/MailContext.jsx
+++ src/contexts/MailContext.jsx
@@ -1,10 +1,35 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { fakeFetch } from "../api/fakefetch";
 
 export const MailContext = createContext();
 
 export const MailProvider = ({children}) =>{
+
+    const [emails, setEmails] = useState([]);
+    const [sentEmails, setSentEmails] = useState([]);
+
+    const getData = async() =>{
+        try {
+            const response = await fakeFetch('https://example.com/api/allemails');
+            if(response.status === 200){
+                setEmails(response.data.emails);
+                setSentEmails(response.data.sentEmails);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(()=>{
+        getData();
+    },[])
+
+    const handleReadBtn = (emailToUpdate) =>{
+        setEmails((emails)=> emails.map((email)=> email.id===emailToUpdate.id ? {...email, read: true} : email))
+    }
+
     return(
-        <MailContext.Provider value={{}}>
+        <MailContext.Provider value={{emails, sentEmails, handleReadBtn}}>
             {children}
         </MailContext.Provider>
     )
--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -1,7 +1,29 @@
+import { useMails } from "../contexts/MailContext";
+import { NavLink } from "react-router-dom";
+
 const Inbox = () =>{
+    const {emails, handleReadBtn} = useMails();
+
+    const readMailsCount = emails.reduce((acc, {read})=> read ? acc+1 : acc,0)
+
     return(
         <>
             <h1>Inbox Page</h1>
+            <p>Unread Emails: {emails.length - readMailsCount}</p>
+            <p>Read Emails: {readMailsCount}</p>
+            <ul>
+                {
+                    emails.map((email)=>{
+                        const {id, sender, subject, message, read} = email;
+                        return(
+                            <li>
+                                <NavLink>{subject}</NavLink>
+                                { !read && (<button onClick={()=>handleReadBtn(email)}>Mark as read</button>)}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </>
     )
 }
